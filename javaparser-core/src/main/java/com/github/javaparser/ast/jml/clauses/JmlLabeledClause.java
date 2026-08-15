05c174b8d6a164e19794ab74e6d3077028cc82fc
Alexander Weigl <weigl@kit.edu>
2026-08-15 19:46:22 +0200

    regnerate

--- javaparser-core/src/main/java/com/github/javaparser/ast/jml/clauses/JmlLabeledClause.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/jml/clauses/JmlLabeledClause.java
@@ -257,6 +257,7 @@ public class JmlLabeledClause extends JmlClause implements NodeWithExpression<Jm
     }
 
     @Override
+    @Generated("com.github.javaparser.generator.core.node.GetMetaModelGenerator")
     public JmlLabeledClauseMetaModel getMetaModel() {
         return JavaParserMetaModel.jmlLabeledClauseMetaModel;
     }
